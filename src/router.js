e707a466346884809adc423a25582f9e7e517c36
Eduardo Ugolini <edu.ugolini2@gmail.com>
2018-10-16 12:34:20 -0300

    Adicionado menu principal

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import DefaultMenu from './views/DefaultMenu.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/default-menu',
+      name: 'default-menu',
+      component: DefaultMenu
     },
     {
       path: '/about',
---
+++ src/views/DefaultMenu.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <div class="pb-2" :style="cssProps">
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-flex
+                align-center
+                justify-center
+                layout
+                text-xs-center
+                class="mb-2">
+                <v-icon x-large>far fa-user</v-icon>
+            </v-flex>
+            <p class="subheading font-weight-light text-md-center text-sm-center text-xs-center mt-0 mb-0">Você está a 3 dias sem treinar</p>
+        </div>
+        <v-tabs
+            slot="extension"
+            v-model="model"
+            grow
+            color="primary"
+            slider-color="secondary"
+        >
+            <v-tab v-for="tab in tabs">
+            <v-icon>{{ tab.icon }}</v-icon>
+            </v-tab>
+        </v-tabs>
+
+        <v-tabs-items v-model="model">
+            <v-tab-item
+                v-for="i in 3"
+                :id="`tab-${i}`"
+                :key="i"
+            >
+                <v-card flat>
+                    <v-card-text v-text="text"></v-card-text>
+                </v-card>
+            </v-tab-item>
+        </v-tabs-items>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            cssProps () {
+                return {
+                    'background-color': this.$vuetify.theme.primary
+                }
+            }
+        },
+        data() {
+            return {
+                tabs: [
+                    {
+                        icon: 'fas fa-dumbbell'    
+                    },
+                    {
+                        icon: 'fas fa-chart-pie'    
+                    },
+                    {
+                        icon: 'fas fa-utensils'    
+                    }
+                ]
+            }
+        }
+    }
+</script>
