62e1683fd5369e8a83ba07dd4b8ac7f0d58edc59
Eduardo Ugolini <edu.ugolini2@gmail.com>
2018-10-16 22:58:34 -0300

    Adicionada versão aceitável da tela de nutrição

---
+++ src/components/GraphicsComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-layout>
+        <v-flex xs5 sm5 class="ml-4 mt-3">
+            <v-card class="elevation-12 pb-5 mb-5">
+                <v-card-title>
+                <div class="pb-3">
+                    <span class="subheading text-xs-center">Peso</span>
+                </div>
+                </v-card-title>
+                <v-content class="mt-2 mb-3" style="text-align: center">
+                    <v-icon x-large>fas fa-chart-pie</v-icon>
+                </v-content>
+            </v-card>
+        </v-flex>
+        <v-flex xs5 sm5 class="ml-4 mt-3">
+            <v-card class="elevation-12 pb-5 mb-5">
+                <v-card-title>
+                <div class="pb-3">
+                    <span class="subheading text-xs-center">Força</span>
+                </div>
+                </v-card-title>
+                <v-content class="mt-2 mb-3" style="text-align: center">
+                    <v-icon x-large>fas fa-chart-pie</v-icon>
+                </v-content>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        name: 'GraphicsComponent',
+        data() {
+            return {
+            }
+        }
+    }
+</script>
---
+++ src/components/NutritionComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-content>
+        <v-container>
+            <v-layout wrap>
+                <v-flex xs12>
+                    <br>
+                    <span class="subheading">Calorias restantes: {{ remainingCalories }}</span><br><br><br>
+                    <span class="subheading mt-1">Carboidratos</span><br>
+                    <v-progress-linear class="mt-1" v-model="carbs"></v-progress-linear><br>
+                    <span class="subheading">Proteínas</span><br>
+                    <v-progress-linear class="mt-1" v-model="proteins"></v-progress-linear><br>
+                    <span class="subheading">Gorduras</span><br>
+                    <v-progress-linear class="mt-1" v-model="fats"></v-progress-linear><br>
+                </v-flex>
+                <v-footer>
+                    <v-flex
+                            align-center
+                            justify-center
+                            layout
+                            text-xs-center
+                            class="mt-3 mb-5 pt-0 pb-0">
+                        <v-icon color="primary">fas fa-plus-circle</v-icon>
+                        <p class="subheading font-weight-light text-md-center text-sm-center text-xs-center ml-1 mt-0 mb-0">Adicionar refeição</p>
+                    </v-flex>
+                </v-footer>
+            </v-layout>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+    export default {
+        name: 'NutritionComponent',
+        data() {
+            return {
+                remainingCalories: 2510,
+                carbs: 70,
+                proteins: 45,
+                fats: 80
+            }
+        }
+    }
+</script>
